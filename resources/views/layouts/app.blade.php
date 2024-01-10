013f236433741be69aaf6633c2532a22bb3b78e4
Mark Ian <bautistamarkian083@gmail.com>
2024-01-10 16:34:11 +0800

    comment out users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,9 +81,9 @@
                     <li>
                         <a href="{{ url('/supplier') }}"><i class="fa fa-truck"></i><span class="nav-label">Suppliers</span></a>
                     </li>
-                    <li>
+                    <!-- <li>
                         <a href="{{ url('/user') }}"><i class="fa fa-users" aria-hidden="true"></i><span class="nav-label">Users</span></a>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
         </nav>
