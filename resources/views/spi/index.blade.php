db9dbf23510bd628f4ddefe680698310952eaa19
Mark Ian <bautistamarkian083@gmail.com>
2024-01-18 15:28:26 +0800

    change for start_date

--- resources/views/spi/index.blade.php
+++ resources/views/spi/index.blade.php
@@ -19,8 +19,8 @@
                                     @csrf
                                     <div class="row mt-10 mb-10">
                                         <div class="col-md-offset-5 col-md-3">
-                                            <label for="start_date">Start Date:</label>
-                                            <input type="date" class="form-control" id="start_date" name="start_date" value="{{ isset($start_date) ? $start_date->format('Y-m-d') : '' }}">
+                                            <label>Start Date:</label>
+                                            <input type="date" class="form-control" name="start_date" value="{{ isset($start_date) ? $start_date : '' }}">
                                         </div>
                                         <div class="col-md-3">
                                             <label>End Date:</label>
