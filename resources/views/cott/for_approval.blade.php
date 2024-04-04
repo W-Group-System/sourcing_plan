da935ad33cae2dc0aa5e486592369802d4f73d31
Mark Ian <bautistamarkian083@gmail.com>
2024-04-04 18:31:24 +0800

    for approval pdf

--- resources/views/cott/for_approval.blade.php
+++ resources/views/cott/for_approval.blade.php
@@ -151,8 +151,8 @@
                             </thead>
                             <tbody>
                                 @php
-                                    $cotts_data = $cotts->whereIn('status', [1, 0])->orderBy('status', 'desc')->get();
-                                    $cot = array_key_last($cotts_data->toArray());
+                                    $cotts_data = $cotts->whereIn('status', [1, 0])->get()->sortByDesc('status');
+                                    $cot = $cotts_data->keys()->last();
                                     $total = 0;
                                     $cost_total = 0;
                                 @endphp
