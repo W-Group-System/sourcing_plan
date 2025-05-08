4387512d32c10a77186acd7f9f0a832e989762c9
JunJihad <junjihadbarroga@gmail.com>
2025-05-08 12:02:15 +0800

    favicon

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -23,6 +23,7 @@
 
     <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{URL::asset('/images/menu3.png')}}">
 
 </head>
 
